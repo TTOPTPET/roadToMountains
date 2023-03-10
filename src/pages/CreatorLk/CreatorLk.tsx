2b1219f40c99fc335f36568b182c0a95dc3e641f
forgetsmd <forgetsmd@gmail.com>
2023-03-10 16:01:56 +0300

    запросы

--- src/pages/CreatorLk/CreatorLk.tsx
+++ src/pages/CreatorLk/CreatorLk.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { getMyTours } from "../../submitFunctions/creatorAPI";
+
+export interface IMyTours {
+  tourId: string;
+  tourName: string;
+  category: string;
+  complexity: string;
+  price: {
+    from: number;
+    to: number;
+  };
+  region: string;
+  tourDate: {
+    from: string;
+    to: string;
+  };
+  personsNumber: number;
+}
 
 function CreatorLk() {
-  return <div>CreatorLk</div>;
+  const [myTours, setMyTours] = useState<Array<IMyTours>>();
+
+  useEffect(() => {
+    getMyTours((value) => setMyTours(value), undefined, true);
+  }, []);
+  return <div>{JSON.stringify(myTours)}</div>;
 }
 
 export default CreatorLk;
