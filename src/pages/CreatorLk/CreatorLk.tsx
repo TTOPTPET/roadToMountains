766c09d073d6d04665a89f352632364aa34ccd1e
forgetsmd <forgetsmd@gmail.com>
2023-03-10 18:10:54 +0300

    убрал лишнее

--- src/pages/CreatorLk/CreatorLk.tsx
+++ src/pages/CreatorLk/CreatorLk.tsx
@@ -20,43 +20,11 @@ export interface IMyTours {
 
 function CreatorLk() {
   const [myTours, setMyTours] = useState<IMyTours[]>();
-  const [def, setDef] = useState<any>();
 
   useEffect(() => {
     getMyTours((value) => setMyTours(value), undefined, true);
-  }, [myTours, def]);
-  return (
-    <>
-      <div>{JSON.stringify(myTours)}</div>
-      <div
-        className="btn"
-        style={{ marginTop: 200 }}
-        onClick={() => {
-          console.log("first");
-          setDef([
-            {
-              tourId: 2,
-              tourName: "aaa",
-              category: "aaa",
-              complexity: "aaa",
-              price: {
-                from: 2,
-                to: 2,
-              },
-              region: "aaa",
-              tourDate: {
-                from: "aaa",
-                to: "aaa",
-              },
-              personsNumber: 2,
-            },
-          ]);
-        }}
-      >
-        OOOOO
-      </div>
-    </>
-  );
+  }, []);
+  return <div>{JSON.stringify(myTours)}</div>;
 }
 
 export default CreatorLk;
