de77c4c681347f5288cea072c581bd88a1bcd686
An1mebit <trinal.fool@gmail.com>
2023-03-18 02:06:56 +0300

    Сверстал первую страницу тура, без стилей

--- src/pages/AddTourPage/AddTourDescription/AddTourDescription.tsx
+++ src/pages/AddTourPage/AddTourDescription/AddTourDescription.tsx
@@ -1,15 +1,53 @@
-import { Stack, Typography } from "@mui/material";
+import {
+  FormControlLabel,
+  Grid,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  Select,
+  Stack,
+  SvgIcon,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { AddTourImage } from "../../../components/AddTourImage/AddTourImage";
 import { useContext, useState } from "react";
 import { RoutingContext, pagesMap } from "../AddTourRouting/AddTourRouting";
+import { IAddTourProps } from "../AddTourPage";
+import { ReactComponent as AttentionIcon } from "../../../media/Attention.svg";
 
-export const AddTourDescription = () => {
+const regions = [
+  "Регион РФ",
+  "Алтайский край",
+  "Владимирская область",
+  "Иркутская область",
+  "Москва",
+  "Самарская область",
+  "Тверская область",
+];
+
+const tourCategory = [
+  "Джип-тур",
+  "Кемпинг",
+  "Конные прогулки",
+  "Рафтинг",
+  "Треккинг",
+  "Хайкинг",
+  "Экскурсия",
+];
+
+export const AddTourDescription = ({
+  handlerTextFieldChange,
+  handlerSelectChange,
+  handlerChangeAge,
+  addTourData,
+}: IAddTourProps) => {
   const [images, setImage] = useState<any[]>([]);
 
   const { page, setPage } = useContext(RoutingContext);
 
   return (
-    <Stack>
+    <Stack gap={1}>
       {page}
       <Stack direction={"row"} justifyContent={"space-between"}>
         <Typography
@@ -28,7 +66,108 @@ export const AddTourDescription = () => {
         </Typography>
       </Stack>
 
-      <AddTourImage images={images} setImage={setImage} />
+      <TextField
+        sx={{ width: 500, height: 50 }}
+        label={"Название тура"}
+        onChange={(e) => handlerTextFieldChange("tourName", e)}
+      />
+
+      <Grid container justifyContent={"space-between"} direction={"row"}>
+        <Grid item xs={6}>
+          <AddTourImage images={images} setImage={setImage} />
+          <Typography component={"h5"}> Описание</Typography>
+          <TextField
+            sx={{ width: 500, height: 110 }}
+            label={"Описание тура (не более 2500 символов)"}
+            onChange={(e) => handlerTextFieldChange("tourDescription", e)}
+            InputProps={{ inputProps: { maxLength: 2500 } }}
+          />
+        </Grid>
+        <Grid container direction={"column"} width={"50%"}>
+          <Grid item>
+            <Stack
+              padding={2}
+              gap={1}
+              direction={"row"}
+              width={464}
+              height={136}
+              borderRadius={5}
+              boxShadow={4}
+            >
+              <SvgIcon viewBox="0 0 33.33 33.33" fontSize="large">
+                <AttentionIcon />
+              </SvgIcon>
+              <Typography component={"p"} width={"auto"}>
+                Обращаем Ваше внимание, что все <br />
+                изменения будут применены только <br />
+                к предстоящим записям. Забронированные <br /> туры обслуживаются
+                по старому тарифу.
+              </Typography>
+            </Stack>
+          </Grid>
+
+          <Grid container direction={"row"} marginTop={5}>
+            <Grid item xs={5}>
+              <Typography component={"h5"}>Регион проведения</Typography>
+              <Select
+                value={addTourData.region}
+                onChange={(e) => handlerSelectChange("region", e)}
+              >
+                {regions.map((region, index) => (
+                  <MenuItem value={region} key={index}>
+                    {region}
+                  </MenuItem>
+                ))}
+              </Select>
+            </Grid>
+            <Grid item xs={5}>
+              <Typography component={"h5"}>Категория тура</Typography>
+              <RadioGroup
+                value={addTourData.category}
+                onChange={(e) => handlerTextFieldChange("category", e)}
+              >
+                {tourCategory.map((category, index) => (
+                  <FormControlLabel
+                    key={index}
+                    value={category}
+                    control={<Radio />}
+                    label={category}
+                  />
+                ))}
+              </RadioGroup>
+            </Grid>
+          </Grid>
+          <Grid item marginTop={1}>
+            <Typography component={"h5"}>Рекомендуемый возраст</Typography>
+            <Stack direction={"row"} marginTop={2}>
+              <TextField
+                sx={{ width: 105 }}
+                InputProps={{ inputProps: { min: 0 } }}
+                type={"number"}
+                label={"От"}
+                onChange={(e) => handlerChangeAge("from", e)}
+              />
+              <TextField
+                sx={{ width: 105 }}
+                InputProps={{ inputProps: { min: 0 } }}
+                type={"number"}
+                label={"До"}
+                onChange={(e) => handlerChangeAge("to", e)}
+              />
+            </Stack>
+          </Grid>
+          <Grid item marginTop={1}>
+            <Typography component={"h5"}>Стоимость тура на человека</Typography>
+            <TextField
+              sx={{ width: 220, marginTop: 2 }}
+              type={"number"}
+              InputProps={{ inputProps: { min: 0 } }}
+              label={"Стоимость тура"}
+              onChange={(e) => handlerTextFieldChange("price", e)}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
     </Stack>
   );
 };
--- src/pages/AddTourPage/AddTourPage.tsx
+++ src/pages/AddTourPage/AddTourPage.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { ChangeEvent, useContext, useState } from "react";
 import AddTourRouting, {
   RoutingContext,
   pagesMap,
@@ -7,15 +7,72 @@ import { AddTourDescription } from "./AddTourDescription/AddTourDescription";
 import { AddTourTravel } from "./AddTourTravel/AddTourTravel";
 import { AddTourControl } from "./AddTourControl/AddTourControl";
 import { AddTourDefault } from "./AddTourDefault/AddTourDefault";
+import { IAddTour, IRecomendedAge } from "../../models/addTourModels/IAddTour";
+import { SelectChangeEvent } from "@mui/material";
+
+export interface IAddTourProps {
+  handlerTextFieldChange: (
+    key: keyof IAddTour,
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  handlerSelectChange: (key: keyof IAddTour, e: SelectChangeEvent) => void;
+  handlerChangeAge: (
+    key: keyof IRecomendedAge,
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  addTourData: IAddTour;
+}
+
+const tourDefault: IAddTour = {
+  tourName: "",
+  tourDescription: "",
+  category: "",
+  region: "Регион РФ",
+  recomendedAge: undefined,
+  price: 0,
+};
 
 const AddTourPageContent = () => {
+  const [addTourData, setAddTourData] = useState<IAddTour>(tourDefault);
+  const [age, setAge] = useState<IRecomendedAge>(null);
+
+  const handlerTextFieldChange = (
+    key: keyof IAddTour,
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setAddTourData({ ...addTourData, [key]: e.target.value });
+    console.log(addTourData);
+  };
+
+  const hadnlerChangeAge = (
+    key: keyof IRecomendedAge,
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setAge({ ...age, [key]: e.target.value });
+    setAddTourData({
+      ...addTourData,
+      recomendedAge: { ...age, [key]: +e.target.value },
+    });
+  };
+
+  const handlerSelectChange = (key: keyof IAddTour, e: SelectChangeEvent) => {
+    setAddTourData({ ...addTourData, [key]: e.target.value });
+  };
+
   const { page } = useContext(RoutingContext);
   return (
     <>
       {!Object.values(pagesMap).includes(page as pagesMap) && (
         <AddTourDefault />
       )}
-      {pagesMap.description === page && <AddTourDescription />}
+      {pagesMap.description === page && (
+        <AddTourDescription
+          handlerTextFieldChange={handlerTextFieldChange}
+          handlerSelectChange={handlerSelectChange}
+          handlerChangeAge={hadnlerChangeAge}
+          addTourData={addTourData}
+        />
+      )}
       {pagesMap.travel === page && <AddTourTravel />}
       {pagesMap.control === page && <AddTourControl />}
     </>
