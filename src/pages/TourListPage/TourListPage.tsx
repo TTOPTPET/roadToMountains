fec4070ad5cfe86a0ea5883aa6fe0b786cd5bbe1
An1mebit <trinal.fool@gmail.com>
2023-03-27 23:33:27 +0300

    Добавил апи, модель для респонса и вывод карточек

--- src/pages/TourListPage/TourListPage.tsx
+++ src/pages/TourListPage/TourListPage.tsx
@@ -5,6 +5,10 @@ import { setModalActive } from "../../redux/Modal/ModalReducer";
 import { BasicFilter, ComplexFilter, Sorter } from "../../components/TourList";
 import { IFilter } from "../../models/tourListModels/IFilter";
 import { getFilters } from "../../submitFunctions/tourListAPI";
+import { ISearchRequest } from "../../models/tourListModels/ISearchRequest";
+import { getToursSorted } from "../../submitFunctions/tourListAPI/searchAPI/searchAPI";
+import { ITour } from "../../models/tourCardModel/ITour";
+import TourCard from "../../components/TourCard/TourCard";
 
 const filterDefault: IFilter = {
   regions: [],
@@ -13,12 +17,42 @@ const filterDefault: IFilter = {
   maxPrice: 0,
 };
 
+const searchDefault: ISearchRequest = {
+  searchParam: "",
+  category: [],
+  tourDuration: {
+    from: 0,
+    to: 0,
+  },
+  complexity: "",
+  price: {
+    from: 0,
+    to: 0,
+  },
+  recommendedAge: {
+    from: 0,
+    to: 0,
+  },
+  regions: [],
+  tourDate: {
+    from: "",
+    to: "",
+  },
+  maxPersonNumber: 0,
+};
+
 function TourListPage() {
   const [filters, setFilters] = useState<IFilter>(filterDefault);
+  const [searchData, setSearchData] = useState<ISearchRequest>(searchDefault);
+  const [tours, setTours] = useState<ITour[]>([]);
 
   useEffect(() => {
     getFilters((filter) => setFilters(filter), undefined, true);
   }, []);
+
+  useEffect(() => {
+    getToursSorted((search) => setTours(search), searchData, undefined, true);
+  }, [searchData]);
 
   const dispatch = useDispatch();
   return (
@@ -46,6 +80,21 @@ function TourListPage() {
       <BasicFilter {...filters} />
       <ComplexFilter {...filters} />
       <Sorter />
+
+      <Stack
+        flexDirection={"row"}
+        flexWrap={"wrap"}
+        gap={2}
+        justifyContent={"center"}
+        marginTop={1}
+      >
+        {tours &&
+          tours
+            .filter((tour) => tour.banStatus !== true)
+            .map((tour, index) => (
+              <TourCard key={index} tour={tour} tourCardType={"tourList"} />
+            ))}
+      </Stack>
     </Stack>
   );
 }
