3bcde4da407451a50d1fb277192d0a9359874ee8
forgetsmd <forgetsmd@gmail.com>
2023-04-03 13:52:45 +0300

    добавил datePicker

--- src/components/TourList/BasicFilter/BasicFilter.tsx
+++ src/components/TourList/BasicFilter/BasicFilter.tsx
@@ -2,6 +2,9 @@ import { ChangeEvent, FC } from "react";
 import { Autocomplete, Button, Stack, TextField } from "@mui/material";
 import { IFilterProps } from "../FilterTypes/IFilterProps";
 import { ISearchRequest } from "../../../models/tourListModels/ISearchRequest";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import dayjs, { Dayjs } from "dayjs";
 
 export const BasicFilter: FC<IFilterProps> = ({
   filters,
@@ -17,27 +20,29 @@ export const BasicFilter: FC<IFilterProps> = ({
     setSearchData({ ...searchData, [key]: e });
   };
 
-  const handleDateChange = (
-    type: "from" | "to",
-    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    switch (type) {
-      case "from": {
-        setSearchData({
-          ...searchData,
-          tourDate: { ...searchData.tourDate, from: e.target.value },
-        });
-        break;
+  const handleDateChange = (type: "from" | "to", value: Dayjs) => {
+    try {
+      const stringDate = value ? value.toISOString() : "";
+      switch (type) {
+        case "from": {
+          setSearchData({
+            ...searchData,
+            tourDate: { ...searchData.tourDate, from: stringDate },
+          });
+          break;
+        }
+        case "to": {
+          setSearchData({
+            ...searchData,
+            tourDate: { ...searchData.tourDate, to: stringDate },
+          });
+          break;
+        }
+        default:
+          break;
       }
-      case "to": {
-        setSearchData({
-          ...searchData,
-          tourDate: { ...searchData.tourDate, to: e.target.value },
-        });
-        break;
-      }
-      default:
-        break;
+    } catch (error) {
+      console.log("invalid Data format");
     }
   };
 
@@ -60,7 +65,35 @@ export const BasicFilter: FC<IFilterProps> = ({
           />
         )}
       />
-      <TextField
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker
+          value={dayjs(searchData.tourDate.from)}
+          onChange={(newValue) => handleDateChange("from", newValue)}
+          renderInput={(props) => (
+            <TextField
+              {...props}
+              inputProps={{
+                ...props.inputProps,
+                placeholder: "Дата заезда",
+              }}
+            />
+          )}
+        />
+        <DatePicker
+          value={dayjs(searchData.tourDate.to)}
+          onChange={(newValue) => handleDateChange("to", newValue)}
+          renderInput={(props) => (
+            <TextField
+              {...props}
+              inputProps={{
+                ...props.inputProps,
+                placeholder: "Дата выезда",
+              }}
+            />
+          )}
+        />
+      </LocalizationProvider>
+      {/* <TextField
         type="date"
         placeholder="Дата заезда"
         value={searchData.tourDate.from}
@@ -71,7 +104,7 @@ export const BasicFilter: FC<IFilterProps> = ({
         placeholder="Дата выезда"
         value={searchData.tourDate.to}
         onChange={(e) => handleDateChange("to", e)}
-      />
+      /> */}
       <TextField
         type={"number"}
         InputProps={{ inputProps: { min: 0 } }}
