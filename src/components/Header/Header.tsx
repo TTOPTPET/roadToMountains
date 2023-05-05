8fb90a0ade6136975f853501931b38f3e0a48805
Maxim <ttoptpet@mail.ru>
2023-05-05 18:43:43 +0300

    хедер без кнопок

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,14 +4,20 @@ import { RootState } from "../../redux/store";
 import logo from "../../media/logo.svg";
 import { UserType } from "../../models/userModels/IUserInfo";
 import { Link, useNavigate } from "react-router-dom";
+import { Cookies } from "react-cookie";
+import { TOKEN } from "../../config/types";
 
 import accIcon from "../../media/accountLinkIcon.svg";
 import calendarIcon from "../../media/calendarIcon.svg";
 
 const Header = () => {
+  const cookie = new Cookies();
+
   const userInfo = useSelector((state: RootState) => state.userInfo.userInfo);
 
   const navigate = useNavigate();
+
+  console.log(cookie.get(TOKEN));
 
   return (
     <Paper variant="header" sx={{ width: "100%" }}>
@@ -32,14 +38,16 @@ const Header = () => {
         >
           <Box
             onClick={() => {
-              navigate("/tours/all");
+              if (cookie.get(TOKEN)) {
+                navigate("/tours/all");
+              }
             }}
             sx={{
               display: "flex",
               alignItems: "center",
               gap: "10px",
               justifyContent: "flex-start",
-              cursor: "pointer",
+              cursor: cookie.get(TOKEN) ? "pointer" : "",
             }}
             className="header__logo"
           >
@@ -54,70 +62,88 @@ const Header = () => {
               Путь <br /> в Горы
             </Typography>
           </Box>
-          {userInfo.typeUser === UserType.tourist && (
-            <Box sx={{ m: "0 30px", width: "890px" }}>
-              <TextField
-                placeholder="Найти тур"
-                color="secondary"
-                // onChange={(e) =>
-                //   dispatch(
-                //
-                //   )
-                // }
-              />
-            </Box>
-          )}
-
-          <Box
-            className="header__btns"
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              gap: "11px",
-              justifyContent: "flex-end",
-            }}
-          >
-            {userInfo.typeUser === UserType.tourist ? (
-              <Box component={Link} to="/tourist/lk">
-                <img style={{ height: "30px" }} src={accIcon} alt="accIcon" />
-              </Box>
-            ) : (
-              <Box sx={{ display: "flex", gap: "22px", alignItems: "center" }}>
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    textDecoration: "none",
-                  }}
-                  component={Link}
-                  to="/creator/calendar"
-                >
-                  <img
-                    style={{ height: "30px" }}
-                    src={calendarIcon}
-                    alt="calendarIcon"
+          {cookie.get(TOKEN) && (
+            <>
+              {userInfo.typeUser === UserType.tourist && (
+                <Box sx={{ m: "0 30px", width: "890px" }}>
+                  <TextField
+                    placeholder="Найти тур"
+                    color="secondary"
+                    // onChange={(e) =>
+                    //   dispatch(
+                    //
+                    //   )
+                    // }
                   />
-                  <Typography variant="caption">Календарь</Typography>
                 </Box>
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    textDecoration: "none",
-                  }}
-                  component={Link}
-                  to="/creator/lk"
-                >
-                  <img src={accIcon} alt="accIcon" style={{ height: "30px" }} />
-                  <Typography variant="caption" sx={{ mt: "1px" }}>
-                    Профиль
-                  </Typography>
-                </Box>
+              )}
+
+              <Box
+                className="header__btns"
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "11px",
+                  justifyContent: "flex-end",
+                }}
+              >
+                {userInfo.typeUser === UserType.tourist ? (
+                  <Box component={Link} to="/tourist/lk">
+                    <img
+                      style={{ height: "30px" }}
+                      src={accIcon}
+                      alt="accIcon"
+                    />
+                  </Box>
+                ) : (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      gap: "22px",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Box
+                      sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        textDecoration: "none",
+                      }}
+                      component={Link}
+                      to="/creator/calendar"
+                    >
+                      <img
+                        style={{ height: "30px" }}
+                        src={calendarIcon}
+                        alt="calendarIcon"
+                      />
+                      <Typography variant="caption">Календарь</Typography>
+                    </Box>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        textDecoration: "none",
+                      }}
+                      component={Link}
+                      to="/creator/lk"
+                    >
+                      <img
+                        src={accIcon}
+                        alt="accIcon"
+                        style={{ height: "30px" }}
+                      />
+                      <Typography variant="caption" sx={{ mt: "1px" }}>
+                        Профиль
+                      </Typography>
+                    </Box>
+                  </Box>
+                )}
               </Box>
-            )}
-          </Box>
+            </>
+          )}
         </Container>
       </Box>
     </Paper>
