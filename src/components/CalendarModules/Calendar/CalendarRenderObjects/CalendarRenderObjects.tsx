3a1250c294ffaf3f6ca8583462ed677ffd173f29
forgetsmd <forgetsmd@gmail.com>
2023-05-10 15:05:37 +0300

    делаю отображение календаря

--- src/components/CalendarModules/Calendar/CalendarRenderObjects/CalendarRenderObjects.tsx
+++ src/components/CalendarModules/Calendar/CalendarRenderObjects/CalendarRenderObjects.tsx
@@ -1,5 +1,77 @@
-type Props = {};
+import dayjs, { Dayjs } from "dayjs";
+import { IPublicTour } from "../../../../models/calendarModels/IPublicTour";
+import { useState, useEffect } from "react";
+import { Box, Paper, Typography } from "@mui/material";
+import {
+  darkBlueColor,
+  darkTurquoiseColor,
+  lightTurquoiseColor,
+} from "../../../../config/MUI/color/color";
 
-export default function CalendarRenderObjects({}: Props) {
-  return <div></div>;
+type Props = { publicTour: IPublicTour[]; date: Dayjs };
+
+export default function CalendarRenderObjects({ publicTour, date }: Props) {
+  const [toursThisDay, setToursThisDay] = useState<IPublicTour[]>([]);
+
+  useEffect(() => {
+    publicTour.forEach((tour) => {
+      if (date.isBetween(tour.tourDate.from, tour.tourDate.to, "date", "[]")) {
+        setToursThisDay((tours) => tours.concat(tour));
+      }
+    });
+    return () => {
+      setToursThisDay([]);
+    };
+  }, [publicTour, date]);
+
+  return (
+    <Box
+      key={date.toString()}
+      sx={{
+        position: "absolute",
+        top: "32px",
+        left: 0,
+        width: "calc(100% + 2px)",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      {toursThisDay.map((tour) => (
+        <>
+          <Box
+            sx={{
+              borderTopLeftRadius:
+                date.isSame(tour.tourDate.from, "D") && "10px",
+              borderBottomLeftRadius:
+                date.isSame(tour.tourDate.from, "D") && "10px",
+              borderTopRightRadius:
+                date.isSame(tour.tourDate.to, "D") && "10px",
+              borderBottomRightRadius:
+                date.isSame(tour.tourDate.to, "D") && "10px",
+              backgroundColor: darkTurquoiseColor,
+              width: "100%",
+              height: "17px",
+              marginBottom: "2px",
+            }}
+          ></Box>
+          {date.isSame(tour.tourDate.from, "D") && (
+            <Typography
+              sx={{
+                position: "absolute",
+                left: "15px",
+                fontSize: "14px",
+                lineHeight: "100%",
+                color: "#ffffff",
+                zIndex: 100,
+                paddingTop: "1px",
+              }}
+            >
+              {tour.tour.tourName}
+            </Typography>
+          )}
+        </>
+      ))}
+    </Box>
+  );
 }
--- src/components/Modals/NewPublicModal/NewPublicModal.tsx
+++ src/components/Modals/NewPublicModal/NewPublicModal.tsx
@@ -103,148 +103,153 @@ export default function NewPublicModal({
       onClose={() => dispatch(setModalInactive("newPublicModal"))}
       open={isModalActive("newPublicModal", activeModals)}
       fullWidth
-      maxWidth={"lg"}
+      maxWidth={"sm"}
     >
-      <DialogContent sx={{ p: "20px 320px" }}>
-        <Typography variant={"h4"} sx={{ mb: "30px", textAlign: "center" }}>
-          Разместить тур
-        </Typography>
+      <DialogContent sx={{ p: "20px" }}>
+        <Box>
+          <Typography variant={"h4"} sx={{ mb: "30px", textAlign: "center" }}>
+            Разместить тур
+          </Typography>
 
-        <Stack direction={"column"} gap={"15px"}>
-          <Autocomplete
-            noOptionsText={"У вас еще нет созданных туров!"}
-            id="tourID"
-            onChange={(e, value) => autocompleteChanged(value)}
-            options={myTours.map((tour) => tour.tourName)}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                placeholder="Выбор тура"
-                color="secondary"
+          <Stack direction={"column"} gap={"15px"}>
+            <Autocomplete
+              noOptionsText={"У вас еще нет созданных туров!"}
+              id="tourID"
+              onChange={(e, value) => autocompleteChanged(value)}
+              options={myTours.map((tour) => tour.tourName)}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  placeholder="Выбор тура"
+                  color="secondary"
+                />
+              )}
+            />
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                value={dayjs(newPublic.tourDate.from)}
+                onChange={(newValue) => {
+                  handleDateChange("from", newValue);
+                }}
+                renderInput={(props) => (
+                  <TextField
+                    color="secondary"
+                    {...props}
+                    error={props.error && props.inputProps.value !== ""}
+                    inputProps={{
+                      ...props.inputProps,
+                      placeholder: "Дата и время начала",
+                    }}
+                  />
+                )}
               />
-            )}
-          />
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker
-              value={dayjs(newPublic.tourDate.from)}
-              onChange={(newValue) => {
-                handleDateChange("from", newValue);
-              }}
-              renderInput={(props) => (
-                <TextField
-                  color="secondary"
-                  {...props}
-                  error={props.error && props.inputProps.value !== ""}
-                  inputProps={{
-                    ...props.inputProps,
-                    placeholder: "Дата и время начала",
-                  }}
-                />
-              )}
-            />
-            <DatePicker
-              value={dayjs(newPublic.tourDate.to)}
-              onChange={(newValue) => handleDateChange("to", newValue)}
-              renderInput={(props) => (
-                <TextField
-                  color="secondary"
-                  {...props}
-                  error={props.error && props.inputProps.value !== ""}
-                  inputProps={{
-                    ...props.inputProps,
-                    placeholder: "Дата и время конца",
-                  }}
-                />
-              )}
-            />
-          </LocalizationProvider>
-          <TextField
-            color="secondary"
-            onChange={(e) =>
-              setNewPublic({ ...newPublic, meetingPoint: e.target.value })
-            }
-            placeholder={"Место встречи"}
-          />
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <TimePicker
-              value={dayjs(newPublic.meetingTime)}
-              ampm={false}
-              onChange={(newValue) =>
-                setNewPublic({ ...newPublic, meetingTime: "12:12:12" })
+              <DatePicker
+                value={dayjs(newPublic.tourDate.to)}
+                onChange={(newValue) => handleDateChange("to", newValue)}
+                renderInput={(props) => (
+                  <TextField
+                    color="secondary"
+                    {...props}
+                    error={props.error && props.inputProps.value !== ""}
+                    inputProps={{
+                      ...props.inputProps,
+                      placeholder: "Дата и время конца",
+                    }}
+                  />
+                )}
+              />
+            </LocalizationProvider>
+            <TextField
+              color="secondary"
+              onChange={(e) =>
+                setNewPublic({ ...newPublic, meetingPoint: e.target.value })
               }
-              renderInput={(props) => (
-                <TextField
-                  color="secondary"
-                  {...props}
-                  error={props.error && props.inputProps.value !== ""}
-                  inputProps={{
-                    ...props.inputProps,
-                    placeholder: "Время встречи",
-                  }}
-                />
-              )}
+              placeholder={"Место встречи"}
             />
-          </LocalizationProvider>
-          <TextField
-            type={"number"}
-            color="secondary"
-            InputProps={{ inputProps: { min: 0 } }}
-            onChange={(e) =>
-              handleFieldChange<number>("maxPersonNum", +e.target.value)
-            }
-            placeholder={"Количество человек"}
-          />
-          <StyledTextAreaAutosize
-            placeholder="Контактная информация"
-            sx={{ m: "0", minHeight: "50px" }}
-            onChange={(e) =>
-              setNewPublic({ ...newPublic, contactInformation: e.target.value })
-            }
-          />
-          <Stack direction={"row"} gap="14px" alignItems={"center"}>
-            <Box>
-              <TextField
-                type={"number"}
-                color="secondary"
-                InputProps={{ inputProps: { min: 0 } }}
-                onChange={(e) =>
-                  handleFieldChange<number>("tourAmount", +e.target.value)
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <TimePicker
+                value={dayjs(newPublic.meetingTime)}
+                ampm={false}
+                onChange={(newValue) =>
+                  setNewPublic({ ...newPublic, meetingTime: "12:12:12" })
                 }
-                placeholder={"Стоимость"}
+                renderInput={(props) => (
+                  <TextField
+                    color="secondary"
+                    {...props}
+                    error={props.error && props.inputProps.value !== ""}
+                    inputProps={{
+                      ...props.inputProps,
+                      placeholder: "Время встречи",
+                    }}
+                  />
+                )}
               />
-            </Box>
+            </LocalizationProvider>
+            <TextField
+              type={"number"}
+              color="secondary"
+              InputProps={{ inputProps: { min: 0 } }}
+              onChange={(e) =>
+                handleFieldChange<number>("maxPersonNum", +e.target.value)
+              }
+              placeholder={"Количество человек"}
+            />
+            <StyledTextAreaAutosize
+              placeholder="Контактная информация"
+              sx={{ m: "0", minHeight: "50px" }}
+              onChange={(e) =>
+                setNewPublic({
+                  ...newPublic,
+                  contactInformation: e.target.value,
+                })
+              }
+            />
+            <Stack direction={"row"} gap="14px" alignItems={"center"}>
+              <Box>
+                <TextField
+                  type={"number"}
+                  color="secondary"
+                  InputProps={{ inputProps: { min: 0 } }}
+                  onChange={(e) =>
+                    handleFieldChange<number>("tourAmount", +e.target.value)
+                  }
+                  placeholder={"Стоимость"}
+                />
+              </Box>
 
-            <Box sx={{ flexGrow: "1" }}>
-              <Typography variant="caption">
-                Стоимость на платформе: <br />
-                {newPublic.tourAmount ? newPublic.tourAmount * 1.05 : "-"}
-              </Typography>
-            </Box>
+              <Box sx={{ flexGrow: "1" }}>
+                <Typography variant="caption">
+                  Стоимость на платформе: <br />
+                  {newPublic.tourAmount ? newPublic.tourAmount * 1.05 : "-"}
+                </Typography>
+              </Box>
+            </Stack>
+
+            <Typography
+              variant="caption"
+              sx={{ mt: "30px", textAlign: "center" }}
+            >
+              Вы можете редактировать тур до 10 марта 2023
+            </Typography>
           </Stack>
 
-          <Typography
-            variant="caption"
-            sx={{ mt: "30px", textAlign: "center" }}
+          <Stack
+            direction={"row"}
+            justifyContent={"center"}
+            marginTop={"20px"}
+            gap={"10px"}
           >
-            Вы можете редактировать тур до 10 марта 2023
-          </Typography>
-        </Stack>
-
-        <Stack
-          direction={"row"}
-          justifyContent={"center"}
-          marginTop={"20px"}
-          gap={"10px"}
-        >
-          <Button onClick={handlerCloseClick}>Отменить</Button>
-          <Button
-            onClick={() => {
-              postNewPublic(newPublic);
-            }}
-          >
-            Разместить тур
-          </Button>
-        </Stack>
+            <Button onClick={handlerCloseClick}>Отменить</Button>
+            <Button
+              onClick={() => {
+                postNewPublic(newPublic);
+              }}
+            >
+              Разместить тур
+            </Button>
+          </Stack>
+        </Box>
       </DialogContent>
     </Dialog>
   );
