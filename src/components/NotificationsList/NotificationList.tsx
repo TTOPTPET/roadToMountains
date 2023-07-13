200b23a221adbd212fec6cc39325cb757dd58322
forgetsmd <forgetsmd@gmail.com>
2023-07-13 16:10:07 +0300

    уведомления привязаны

--- src/components/NotificationsList/NotificationList.tsx
+++ src/components/NotificationsList/NotificationList.tsx
@@ -1,15 +1,12 @@
 import { Stack } from "@mui/system";
-import { useState, useEffect } from "react";
 import { INotification } from "../../models/notificationModels/INotification";
-import { getNotifications } from "../../API/notificationAPI/getNotifications";
 import { Notification } from "./Notification/Notification";
 
-export const NotificationList = () => {
-  const [notifications, setNotifications] = useState<INotification[]>([]);
-  useEffect(() => {
-    getNotifications((value) => setNotifications(value), undefined, true);
-  }, []);
+type Props = {
+  notifications: INotification[];
+};
 
+export const NotificationList = ({ notifications }: Props) => {
   return (
     <Stack width={"100%"} gap={3} marginTop={5}>
       {notifications &&
